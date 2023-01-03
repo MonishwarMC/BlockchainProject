f0832395ca8348e18c731089c7676298706b6362
MonishwarMC <anikmonish@gmail.com>
2023-01-03 15:07:48 +0530

    increament

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -5,10 +5,17 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Counter{
-    uint counter;
-    function count() public returns(uint){
+    uint32 counter;
+    event counterIncreament(uint32 counter);
+    function count() public {
         counter++;
-        console.log("now the count is ", counter);
+        console.log("counter:", counter);
+        emit counterIncreament(counter);
+    }
+    function getCounter() public view returns(uint32){
         return counter;
+    }
+    function hello() public pure returns(string memory){
+        return "Hello";
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -25,12 +25,28 @@ async function run(){
     if((!await hasAccounts()) && (!await requestAccounts())){
         throw new Error("Please check for all dependecies");
     }
-    const hello = new ethers.Contract(
-        "0x5fbdb2315678afecb367f032d93f642f64180aa3",
-        ["function hello() public pure returns(string memory)",],
-        new ethers.providers.Web3Provider(getEth())
+    const counter = new ethers.Contract(
+        process.env.CONTRACT_ADDRESS,
+        [
+            "function hello() public pure returns(string memory)",
+            "function count() public",
+            "function getCounter() public view returns(uint32)"
+        ],
+        new ethers.providers.Web3Provider(getEth()).getSigner()
     )
-    document.body.innerHTML = await hello.hello(); 
-}
 
+    const el = document.createElement("div");
+    async function setCounter() {
+        el.innerHTML = await counter.getCounter();
+    }
+    setCounter();
+    const button = document.createElement("button");
+    button.innerHTML = "increament";
+    button.onclick = async function() {
+        await counter.count();
+    };
+
+    document.body.appendChild(el);
+    document.body.appendChild(button); 
+}
 run();
